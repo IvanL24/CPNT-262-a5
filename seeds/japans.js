813a9d878197dcc5bd3c90f0391ece6db40d006c
Ivan Lavena <lavenaivan@gmail.com>
2020-11-14 03:50:37 -0700

    and  deleted

--- seeds/japans.js
+++ seeds/japans.js
@@ -1,75 +1,75 @@
 const japan = [
   {
-    id:     101,
-    title: 'shrine entrance painting',
-    width:  500,
-    height: 300,
-    description: 'shrine entrance painting',
+    id:           101,
+    title:       'Shrine entrance painting',
+    width:        500,
+    height:       300,
+    description: 'Shrine entrance painting',
 
-    imagePath: `public/images/gallery/101.jpg`
+    imagePath:   `public/images/gallery/101.jpg`
   },
   {
-    id:     102,
-    title: 'alley way',
-    width:  500,
-    height: 300,
-    description: 'alley way',
+    id:           102,
+    title:       'Alley way',
+    width:        500,
+    height:       300,
+    description: 'Alley way',
 
-    imagePath: `public/images/gallery/102.jpg`
+    imagePath:   `public/images/gallery/102.jpg`
   },
   {
-    id:     103,
-    title: 'christmas store front',
-    width:  500,
-    height: 300,
-    description: 'christmas store front',
+    id:           103,
+    title:       'Christmas store front',
+    width:        500,
+    height:       300,
+    description: 'Christmas store front',
 
-    imagePath: `public/images/gallery/103.jpg`
+    imagePath:   `public/images/gallery/103.jpg`
   },
   {
-    id:     104,
-    title: 'rainy night',
-    width:  500,
-    height: 300,
-    description: 'rainy night',
+    id:           104,
+    title:       'Rainy night',
+    width:        500,
+    height:       300,
+    description: 'Rainy night',
 
-    imagePath: `public/images/gallery/104.jpg`
+    imagePath:   `public/images/gallery/104.jpg`
   },
   {
-    id:     105,
-    title: 'night life',
-    width:  500,
-    height: 300,
-    description: 'night life',
+    id:           105,
+    title:       'Night life',
+    width:        500,
+    height:       300,
+    description: 'Night life',
 
-    imagePath: `public/images/gallery/105.jpg`
+    imagePath:   `public/images/gallery/105.jpg`
   },
   {
-    id:     106,
-    title: 'matsumoto castle',
-    width:  500,
-    height: 300,
-    description: 'matsumoto castle',
+    id:           106,
+    title:       'Matsumoto castle',
+    width:        500,
+    height:       300,
+    description: 'Matsumoto castle',
 
-    imagePath: `public/images/gallery/106.jpg`
+    imagePath:   `public/images/gallery/106.jpg`
   },
   {
-    id:     107,
-    title: 'Tokyo Imperial Palace',
-    width:  500,
-    height: 300,
+    id:           107,
+    title:       'Tokyo Imperial Palace',
+    width:        500,
+    height:       300,
     description: 'Tokyo Imperial Palace',
 
-    imagePath: `public/images/gallery/107.jpg`
+    imagePath:   `public/images/gallery/107.jpg`
   },
   {
-    id:     108,
-    title: 'Mount Fuji',
-    width:  500,
-    height: 300,
+    id:           108,
+    title:       'Mount Fuji',
+    width:        500,
+    height:       300,
     description: 'Mount Fuji',
 
-    imagePath: `public/images/gallery/108.jpg`
+    imagePath:   `public/images/gallery/108.jpg`
   },
 ];
 
